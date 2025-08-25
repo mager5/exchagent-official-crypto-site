882eaa5f15765a24ad2c69211a240dcf5969c296
Maximus <mager5@yandex.ru>
2025-08-25 18:47:45 +0300

    🔧 Исправлена маршрутизация для GitHub Pages: добавлен basename в BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? '/exchagent-official-crypto-site' : '/'}>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
